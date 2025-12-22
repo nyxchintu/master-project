f217cfa2e6f4cfa4983ab537d0b2c411a2ee4556
Chintu <85810323+nyxchintu@users.noreply.github.com>
2025-12-22 03:19:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@
 // Get them from: https://console.firebase.google.com/ > Project Settings > General > Your apps
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyBYSudxTRdLyhWcDCaCBRwmg9K7-Acn4l0",
+    authDomain: "master-project-chintunyx.firebaseapp.com",
+    projectId: "master-project-chintunyx",
+    storageBucket: "master-project-chintunyx.firebasestorage.app",
+    messagingSenderId: "654512957232",
+    appId: "1:654512957232:web:2a4ba2f902866912c8caf5"
+  };
 
 // Initialize Firebase (only if not already initialized)
 let db = null;
